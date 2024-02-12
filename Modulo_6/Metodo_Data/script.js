d1c6df13ca2ee9d8c349f1d6b98dbcc57f0ea0b8
João Paulo <pjoao6492@gmail.com>
2024-02-12 08:35:29 -0300

    metodos data

--- Modulo_6/Metodo_Data/script.js
+++ Modulo_6/Metodo_Data/script.js
@@ -1,3 +1,29 @@
-let d = new Date(6000000);
+/*
+let dia = d.getDate();
+let diaSemana = d.getDay(); // começa no domingo com 0(zero)
+let mes = d.getMonth(); // Começa em 0(zero)
+let hora = d.getHours();
+*/
 
-console.log(d.toUTCString())
+let d = new Date();
+
+d.setDate(d.getDate() + 5);
+//let time = d.getTime(); // tempo desde 1970 em miliSeconds
+console.log(d)
+
+function formatNumber(number){
+    return number < 10 ? `0${number}` : `${number}`
+}
+
+function tempo(){
+    setInterval(() => {
+        let d = new Date();
+        let minuto = formatNumber(d.getMinutes());
+        let segundos = formatNumber(d.getSeconds());
+        let miliSeconds = formatNumber(d.getMilliseconds());
+        document.getElementById('tempo').innerHTML = `${minuto}:${segundos}:${miliSeconds}`
+    }, 10)
+
+}
+
+tempo()
